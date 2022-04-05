b8295899bf975f5f645ec5fc9d59dacb0201e24d
Aloysius Chan <aloycwl@gmail.com>
2022-04-05 15:48:45 +0800

    Update NOC-ERC721AC.sol

--- NOC-ERC721AC.sol
+++ NOC-ERC721AC.sol
@@ -20,42 +20,47 @@ interface IERC721Metadata{
     function tokenURI(uint256 tokenId)external view returns(string memory);
 }
 contract ERC721AC is IERC721,IERC721Metadata{
-    uint256 constant WAITINGTIME=0; //[DEPLOYMENT SET TO 604800 (1 WEEK)]
+    uint256 constant WAIT=0; //[DEPLOYMENT SET TO 604800 (1 WEEK)]
     uint256 constant COST=0 ether;  //[DEPLOYMENT SET TO 0.88]
     uint256 public count=0;
-    uint256 percent=5;
-    bool paused=false;
-    bool breedable=true;
+    uint256 private percent=5;
     address private _owner;
     struct AC{
         address owner;
         uint256 parent1;
         uint256 parent2;
         uint256 time;
-        uint256 generation;
-        uint256 gender;
+        uint256 gen;
+        uint256 sex;
     }
-    struct GENERATION{
+    struct GEN{
         uint256 maxCount;
         uint256 currentCount;
+        bool breedable;
+        string[2]baseURI; //sex:0-f/1-m
     }
-    mapping(uint256=>AC)public ac;  //change back to private
-    mapping(uint256=>GENERATION)private age;
-    mapping(address=>uint256[])internal tokens;
-    mapping(uint256=>mapping(uint256=>string))internal baseURIs; //generation: 1 onwards, gender: 1-f/2-m
+    mapping(uint256=>AC)public ac;
+    mapping(uint256=>GEN)public age;
+    mapping(address=>uint256[])private tokens;
     modifier onlyOwner(){
         require(_owner==msg.sender);_;
     }
     constructor(){
         _owner=msg.sender;
-        age[1].maxCount=7;
+        age[1].maxCount=7; //TESTING VARIABLES STARTS
+        age[1].breedable=true;
+        age[1].baseURI[0]="http://somedomain.com/gen1female/";
+        age[1].baseURI[1]="http://somedomain.com/gen1male/";
         age[2].maxCount=16;
+        age[2].breedable=true;
+        age[2].baseURI[0]="http://somedomain.com/gen2female/";
+        age[2].baseURI[1]="http://somedomain.com/gen2male/";
     }
-    function supportsInterface(bytes4 t)external pure returns(bool){
-        return t==type(IERC721).interfaceId||t==type(IERC721Metadata).interfaceId;
+    function supportsInterface(bytes4 _t)external pure returns(bool){
+        return _t==type(IERC721).interfaceId||_t==type(IERC721Metadata).interfaceId;
     }
-    function ownerOf(uint256 tokenId)public view returns(address){
-        return ac[tokenId].owner;
+    function ownerOf(uint256 _c)external view returns(address){
+        return ac[_c].owner;
     }
     function name()external pure override returns(string memory){
         return"Noctural-Owl-Club";
@@ -63,114 +68,118 @@ contract ERC721AC is IERC721,IERC721Metadata{
     function symbol()external pure override returns(string memory){
         return"NOC";
     }
-    function tokenURI(uint256 _t)external view override virtual returns(string memory){
-        uint256 temp=_t;
-        uint256 digits;
-        while(temp!=0){
-            digits++;
-            temp/=10;
+    function tokenURI(uint256 _c)external view override returns(string memory){
+        unchecked{
+            uint256 temp=_c;
+            uint256 digits;
+            while(temp!=0){
+                digits++;
+                temp/=10;
+            }
+            temp=_c;
+            bytes memory buffer=new bytes(digits);
+            while(temp!=0){
+                digits-=1;
+                buffer[digits]=bytes1(uint8(48+uint256(_c%10)));
+                temp/=10;
+            }
+            return string(abi.encodePacked(age[ac[_c].gen].baseURI[ac[_c].sex],_c>0?string(buffer):"0",".json"));
+            
         }
-        temp=_t;
-        bytes memory buffer=new bytes(digits);
-        while(temp!=0){
-            digits-=1;
-            buffer[digits]=bytes1(uint8(48+uint256(_t%10)));
-            temp/=10;
+    }
+    function safeTransferFrom(address _f,address _t,uint256 _c)external override{
+        transferFrom(_f,_t,_c);
+    }
+    function safeTransferFrom(address _f,address _t,uint256 _c,bytes memory _d)external override{
+        transferFrom(_f,_t,_c);
+    }
+    function transferFrom(address _f,address _t,uint256 _c)public override{
+        unchecked{
+            require(_f==msg.sender);
+            for(uint256 i=0;i<tokens[_f].length;i++)if(tokens[_f][i]==_c){
+                tokens[_f][i]=tokens[_f][tokens[_f].length-1];
+                tokens[_f].pop();
+                break;
+            }
+            tokens[_t].push(_c);
+            ac[_c].owner=_t;
         }
-        return string(abi.encodePacked(baseURIs[ac[_t].generation][ac[_t].gender],_t>0?string(buffer):"0",".json"));
+        emit Transfer(_f,_t,_c);
     }
-    function safeTransferFrom(address from,address to,uint256 t)external override{
-        transferFrom(from,to,t);
+    function approve(address _t,uint256 _c)external override{
+        emit Approval(ac[_c].owner,_t,_c);
     }
-    function safeTransferFrom(address from,address to,uint256 t,bytes memory _data)external override{
-        transferFrom(from,to,t);
-    }
-    function transferFrom(address from,address to,uint256 _t)public override{
-        require(from==msg.sender);
-        for(uint256 i=0;i<tokens[from].length;i++)if(tokens[from][i]==_t){
-            tokens[from][i]=tokens[from][tokens[from].length-1];
-            tokens[from].pop();
-            break;
-        }
-        tokens[to].push(_t);
-        ac[_t].owner=to;
-        emit Transfer(from,to,_t);
-    }
-    function approve(address to,uint256 t)external override{
-        emit Approval(ac[t].owner,to,t);
-    }
-    function getApproved(uint256 t)external view override returns(address){
+    function getApproved(uint256 _c)external view override returns(address){
         return msg.sender;
     }
-    function Mint(address _a, uint256 _generation)internal{
+    function _mint(address _a, uint256 _g)private{
         unchecked{
+            require(age[_g].currentCount<age[_g].maxCount&& //not exceeding gen max count
+            age[_g].breedable); //this gen set to breed
             count++;
-            age[_generation].currentCount++;
+            age[_g].currentCount++;
             ac[count].owner=_a;
-            ac[count].gender=
-                uint256(keccak256(abi.encodePacked(block.timestamp)))%10>(_generation==1?4:8)?1:2;
-            ac[count].generation=_generation;
+            ac[count].sex=uint256(keccak256(abi.encodePacked(block.timestamp)))%10>(_g==1?4:8)?0:1;
+            ac[count].gen=_g;
             tokens[_a].push(count);
         }
-        emit Transfer(address(0),msg.sender,age[_generation].currentCount);
+        emit Transfer(address(0),msg.sender,age[_g].currentCount);
         
     }
-    function getOwnerWallet()public view returns(uint256[]memory){
+    function getOwnerWallet()external view returns(uint256[]memory){
         return tokens[msg.sender];
     }
     function ContractWalletBalance()external view returns(uint256){
         return address(this).balance;
     }
-    function setAirdrop(address airdropAddress)external onlyOwner{
-        Mint(airdropAddress,1);
+    function setAirdrop(address _a)external onlyOwner{
+        _mint(_a,1);
     }
-    function setBaseURI(uint256 _generation,uint256 _gender,string memory _str)external onlyOwner{
-        baseURIs[_generation][_gender]=_str;
+    function genPREP(uint256 _c, bool _b, uint256 _x, string memory _f, string memory _m)external onlyOwner{
+        age[_c].breedable=_b;
+        age[_c].maxCount=_x;
+        age[_c].baseURI[0]=_f;
+        age[_c].baseURI[1]=_m;
     }
-    function setBreed(bool _state)external onlyOwner{
-        breedable=_state;
-    }
-    function setDistribute()external{
-        for(uint256 i=1;i<=count;i++){
-            (bool success,)=payable(payable(ownerOf(i))).call{value:address(this).balance/count}("");
-            require(success);
+    function DISTRIBUTE()external{
+        unchecked{
+            for(uint256 i=1;i<=count;i++){
+                payable(payable(ac[i].owner)).call{value:address(this).balance/count}("");
+            }
         }
     }
-    function setPause(bool _state)external onlyOwner{
-        paused=_state;
+    function setPercent(uint256 _p)external onlyOwner(){
+        percent=_p;
     }
-    function setPercent(uint256 _newPercentage)external onlyOwner(){
-        percent=_newPercentage;
-    }
-    function mint()external payable{
-        require(!paused&&age[1].currentCount<=age[1].maxCount&&msg.value>=COST);
-        Mint(msg.sender,1);
-        for(uint256 i=1;i<=count;i++){
-            (bool success,)=payable(payable(ownerOf(i))).call{value:address(this).balance*percent/100/count}("");
-            require(success);
+    function MINT()external payable{
+        unchecked{
+            require(msg.value>=COST);
+            _mint(msg.sender,1);
+            for(uint256 i=1;i<=count;i++){
+                payable(payable(ac[i].owner)).call{value:address(this).balance*percent/100/count}("");
+            }
+            payable(_owner).call{value:address(this).balance}("");
         }
-        (bool os,)=payable(_owner).call{value:address(this).balance}("");
-        require(os);
     }
-    function breed(uint256 p1,uint256 p2)external{
-        bool existed; //never mint before
-        for(uint256 i=0;i<tokens[msg.sender].length;i++){
-            if((ac[tokens[msg.sender][i]].parent1==p1&&ac[tokens[msg.sender][i]].parent2==p2)||
-            (ac[tokens[msg.sender][i]].parent2==p1&&ac[tokens[msg.sender][i]].parent1==p2))existed==true;
+    function BREED(uint256 _1,uint256 _2)external{
+        unchecked{
+            bool existed;
+            for(uint256 i=0;i<tokens[msg.sender].length;i++){
+                if((ac[tokens[msg.sender][i]].parent1==_1&&ac[tokens[msg.sender][i]].parent2==_2)||
+                (ac[tokens[msg.sender][i]].parent2==_1&&ac[tokens[msg.sender][i]].parent1==_2))existed==true;
+            }
+            /*require(!existed&&  //never mint before
+                ac[_1].gen==ac[_2].gen&& //must be same gen
+                ac[_1].owner==msg.sender&&ac[_2].owner==msg.sender&& //must only owner of _1 and _2
+                ac[_1].time+WAIT<block.timestamp&&ac[_2].time+WAIT<block.timestamp&& //parents not minted recently
+                (ac[_1].sex==1&&ac[_2].sex==2)||(ac[_2].sex==1&&ac[_1].sex==2) //must be different sex
+            );*/
+            _mint(msg.sender,ac[_1].gen+1);
+            ac[count].parent1=_1;
+            ac[count].parent2=_2;
+            ac[_1].time=block.timestamp;
+            ac[_2].time=block.timestamp;
         }
-        require(!paused&&
-        breedable&&
-        ac[p1].generation==ac[p2].generation&& //must be same generation
-        age[ac[p1].generation].currentCount<=age[ac[p1].generation].maxCount&& //same generation count not exhausted
-        ownerOf(p1)==msg.sender&&ownerOf(p2)==msg.sender&& //must only owner of p1 and p2
-        ac[p1].time+WAITINGTIME<block.timestamp&&ac[p2].time+WAITINGTIME<block.timestamp&& //parents haven't been minted recently
-        (ac[p1].gender==1&&ac[p2].gender==2)||(ac[p2].gender==1&&ac[p1].gender==2)&& //must be different gender
-        !existed);
-        Mint(msg.sender,2);
-        ac[count].parent1=p1;
-        ac[count].parent2=p2;
-        ac[p1].time=block.timestamp;
-        ac[p2].time=block.timestamp;
     }
   /* 
      UAT Preparation
