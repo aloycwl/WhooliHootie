538db2db4e5274d89e483a1f0ac66a2defb225a5
"Aloysius" <"aloycwl@gmail.com">
2022-04-06 10:50:48 +0800

    Updated breeding requirement

--- NOC-ERC721AC.sol
+++ NOC-ERC721AC.sol
@@ -20,8 +20,8 @@ interface IERC721Metadata{
     function tokenURI(uint256 tokenId)external view returns(string memory);
 }
 contract ERC721AC is IERC721,IERC721Metadata{
-    uint256 constant WAIT=0; //[DEPLOYMENT SET TO 604800 (1 WEEK)]
-    uint256 constant COST=0 ether;  //[DEPLOYMENT SET TO 0.88]
+    uint256 private constant WAIT=0; //[DEPLOYMENT SET TO 604800 (1 WEEK)]
+    uint256 private constant COST=0 ether;  //[DEPLOYMENT SET TO 0.88]
     uint256 public count=0;
     uint256 private percent=5;
     address private _owner;
@@ -40,18 +40,18 @@ contract ERC721AC is IERC721,IERC721Metadata{
         string[2]baseURI; //sex:0-f/1-m
     }
     mapping(uint256=>AC)public ac;
-    mapping(uint256=>GEN)public age;
-    mapping(address=>uint256[])public tokens;
+    mapping(uint256=>GEN)private age;
+    mapping(address=>uint256[])private tokens;
     modifier onlyOwner(){
         require(_owner==msg.sender);_;
     }
     constructor(){
         _owner=msg.sender;
-        age[1].maxCount=7; //TESTING VARIABLES STARTS
+        age[1].maxCount=168; //TESTING VARIABLES STARTS
         age[1].breedable=true;
         age[1].baseURI[0]="http://somedomain.com/gen1female/";
         age[1].baseURI[1]="http://somedomain.com/gen1male/";
-        age[2].maxCount=16;
+        age[2].maxCount=1680;
         age[2].breedable=true;
         age[2].baseURI[0]="http://somedomain.com/gen2female/";
         age[2].baseURI[1]="http://somedomain.com/gen2male/";
@@ -95,7 +95,7 @@ contract ERC721AC is IERC721,IERC721Metadata{
     }
     function transferFrom(address _f,address _t,uint256 _c)public override{
         unchecked{
-            require(_f==msg.sender);
+            require(ac[_c].owner==_f);
             for(uint256 i=0;i<tokens[_f].length;i++)if(tokens[_f][i]==_c){
                 tokens[_f][i]=tokens[_f][tokens[_f].length-1];
                 tokens[_f].pop();
@@ -112,30 +112,19 @@ contract ERC721AC is IERC721,IERC721Metadata{
     function getApproved(uint256 _c)external view override returns(address){
         return msg.sender;
     }
-    function _mint(address _a, uint256 _g)private{
-        unchecked{
-            require(age[_g].currentCount<age[_g].maxCount&& //not exceeding gen max count
-            age[_g].breedable); //this gen set to breed
-            count++;
-            age[_g].currentCount++;
-            ac[count].owner=_a;
-            ac[count].sex=uint256(keccak256(abi.encodePacked(block.timestamp)))%10>(_g==1?4:8)?0:1;
-            ac[count].gen=_g;
-            tokens[_a].push(count);
-        }
-        emit Transfer(address(0),msg.sender,age[_g].currentCount);
-        
-    }
-    function getOwnerWallet()external view returns(uint256[]memory){
+    function getWallet()external view returns(uint256[]memory){
         return tokens[msg.sender];
     }
-    function ContractWalletBalance()external view returns(uint256){
+    function getBalance()external view returns(uint256){
         return address(this).balance;
     }
-    function setAirdrop(address _a)external onlyOwner{
-        _mint(_a,1);
+    function getGenCount(uint256 _g)external view returns(uint256){
+        return age[_g].currentCount;
     }
-    function genPREP(uint256 _c, bool _b, uint256 _x, string memory _f, string memory _m)external onlyOwner{
+    function setPercent(uint256 _p)external onlyOwner(){
+        percent=_p;
+    }
+    function GENPREP(uint256 _c, bool _b, uint256 _x, string memory _f, string memory _m)external onlyOwner{
         age[_c].breedable=_b;
         age[_c].maxCount=_x;
         age[_c].baseURI[0]=_f;
@@ -148,8 +137,20 @@ contract ERC721AC is IERC721,IERC721Metadata{
             }
         }
     }
-    function setPercent(uint256 _p)external onlyOwner(){
-        percent=_p;
+    function _mint(address _a, uint256 _g)private{
+        unchecked{
+            //require(age[_g].currentCount<age[_g].maxCount&&age[_g].breedable); [DEPLOYMENT TO UNCOMMENT]
+            count++;
+            age[_g].currentCount++;
+            ac[count].owner=_a;
+            ac[count].sex=uint256(keccak256(abi.encodePacked(block.timestamp)))%10>(_g==1?4:8)?0:1;
+            ac[count].gen=_g;
+            tokens[_a].push(count);
+        }
+        emit Transfer(address(0),msg.sender,age[_g].currentCount);
+    }
+    function AIRDROP(address _a)external onlyOwner{
+        _mint(_a,1);
     }
     function MINT()external payable{
         unchecked{
@@ -164,19 +165,18 @@ contract ERC721AC is IERC721,IERC721Metadata{
     function BREED(uint256 _1,uint256 _2)external{
         unchecked{
             bool existed;
-            for(uint256 i=tokens[msg.sender].length-1;i>1;i--){
+            for(uint256 i=0;tokens[msg.sender].length>i;i++){
                 if(((ac[tokens[msg.sender][i]].parent1==_1&&ac[tokens[msg.sender][i]].parent2==_2)||
                 (ac[tokens[msg.sender][i]].parent2==_1&&ac[tokens[msg.sender][i]].parent1==_2))){
                     existed=true;
                     break;
                 }
             }
-            require(!existed&&  //never mint before
-                ac[_1].gen==ac[_2].gen);/*&& //must be same gen
+            require(!existed&& //never mint before
+                ac[_1].gen==ac[_2].gen&& //must be same gen
                 ac[_1].owner==msg.sender&&ac[_2].owner==msg.sender&& //must only owner of _1 and _2
-                ac[_1].time+WAIT<block.timestamp&&ac[_2].time+WAIT<block.timestamp&& //parents not minted recently
-                (ac[_1].sex==1&&ac[_2].sex==2)||(ac[_2].sex==1&&ac[_1].sex==2) //must be different sex
-                HOW
+                (ac[_1].sex==0&&ac[_2].sex==1||ac[_2].sex==0&&ac[_1].sex==1)); //must be different sex
+                /*ac[_1].time+WAIT<block.timestamp&&ac[_2].time+WAIT<block.timestamp&& //parents not minted recently
             );*/
             _mint(msg.sender,ac[_1].gen+1);
             ac[count].parent1=_1;
@@ -184,9 +184,9 @@ contract ERC721AC is IERC721,IERC721Metadata{
             ac[_1].time=block.timestamp;
             ac[_2].time=block.timestamp;
         }
-        
     }
-  /* 
+} 
+/* 
      UAT Preparation
      1. test to all 168 gen1 mint + airdrop
      2. test to all 1680 gen2 breed
@@ -205,4 +205,3 @@ contract ERC721AC is IERC721,IERC721Metadata{
      15. distributable (opensea to admin)
      16. minting distributable
   */
-} 
