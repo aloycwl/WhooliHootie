41e754a44ea0db69c637733eae3e81c7a50752ad
"Aloy" <"aloycwl@gmail.com">
2022-04-19 14:20:55 +0800

    Changes to ERC721AC

--- WH2_ERC721.sol
+++ WH2_ERC721.sol
@@ -1,9 +1,95 @@
-pragma solidity^0.8.7;//SPDX-License-Identifier:None
-import"WH2_attachment.sol";
-
-contract WHOOLIHOOTIE is ERC721,Ownable{
+pragma solidity^0.8.0;//SPDX-License-Identifier: MIT
+interface IERC721{
+    event Transfer(address indexed from,address indexed to,uint256 indexed tokenId);
+    event Approval(address indexed owner,address indexed approved,uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner,address indexed operator,bool approved);
+    function balanceOf(address owner)external view returns(uint256 balance);
+    function ownerOf(uint256 tokenId)external view returns(address owner);
+    function safeTransferFrom(address from,address to,uint256 tokenId)external;
+    function transferFrom(address from,address to,uint256 tokenId)external;
+    function approve(address to,uint256 tokenId)external;
+    function getApproved(uint256 tokenId)external view returns(address operator);
+    function setApprovalForAll(address operator,bool _approved)external;
+    function isApprovedForAll(address owner,address operator)external view returns(bool);
+    function safeTransferFrom(address from,address to,uint256 tokenId,bytes calldata data)external;
+}
+interface IERC721Metadata{
+    function name()external view returns(string memory);
+    function symbol()external view returns(string memory);
+    function tokenURI(uint256 tokenId)external view returns(string memory);
+}
+contract ERC721AC is IERC721,IERC721Metadata{
+    address private _owner;
+    mapping(uint256=>address)private _owners;
+    mapping(address=>uint256)private _balances;
+    mapping(uint256=>address)private _tokenApprovals;
+    mapping(address=>mapping(address=>bool))private _operatorApprovals;
+    constructor(){
+        _owner=msg.sender;
+    }
+    function supportsInterface(bytes4 f)external pure returns(bool){
+        return f==type(IERC721).interfaceId||f==type(IERC721Metadata).interfaceId;
+    }
+    function balanceOf(address o)external view override returns(uint256){
+        return _balances[o];
+    }
+    function ownerOf(uint256 k)public view override returns(address){
+        return _owners[k];
+    }
+    function owner()external view returns(address){
+        return _owner;
+    }
+    function name()external pure override returns(string memory){
+        return "_name";
+    }
+    function symbol()external pure override returns(string memory){
+        return "_symbol";
+    }
+    function tokenURI(uint256 k)external pure override returns(string memory){
+        require(k==k);
+        return"";
+    }
+    function approve(address t,uint256 k)external override{
+        require(msg.sender==ownerOf(k)||isApprovedForAll(ownerOf(k),msg.sender));
+        _tokenApprovals[k]=t;
+        emit Approval(ERC721.ownerOf(k),t,k);
+    }
+    function getApproved(uint256 tokenId)public view override returns(address){
+        return _tokenApprovals[tokenId];
+    }
+    function setApprovalForAll(address p,bool a)external override{
+        _operatorApprovals[msg.sender][p]=a;
+        emit ApprovalForAll(msg.sender,p,a);
+    }
+    function isApprovedForAll(address o,address p)public view override returns(bool){
+        return _operatorApprovals[o][p];
+    }
+    function transferFrom(address f,address t,uint256 k)public override{
+        require(f==ownerOf(k)||getApproved(k)==f||isApprovedForAll(ownerOf(k),f));
+        _tokenApprovals[k]=address(0);
+        emit Approval(ERC721.ownerOf(k),t,k);
+        _balances[f]-=1;
+        _balances[t]+=1;
+        _owners[k]=t;
+        emit Transfer(f,t,k);
+    }
+    function safeTransferFrom(address f,address t,uint256 k)external override{
+        transferFrom(f,t,k);
+    }
+    function safeTransferFrom(address f,address t,uint256 k,bytes memory d)external override{
+        require(keccak256(abi.encodePacked(d))==keccak256(abi.encodePacked(d)));
+        transferFrom(f,t,k);
+    }
+    function MINT(address to,uint256 tokenId)public{
+        _balances[to]+=1;
+        _owners[tokenId]=to;
+        emit Transfer(address(0),to,tokenId);
+    }
+}
+contract WHCC is ERC721AC{
     uint256 private percent=5;
-    uint256 public count=0;
+    address private _owner;
+    uint256 public count;
     struct OWL{
         address owner;
         uint256 parent1;
@@ -20,30 +106,58 @@ contract WHOOLIHOOTIE is ERC721,Ownable{
     mapping(uint256=>OWL)public owl;
     mapping(uint256=>GEN)public gen;
     mapping(address=>uint256[])public tokens;
-    constructor(string memory name,string memory symbol) ERC721(name, symbol) {
+    modifier onlyOwner(){
+        require(_owner==msg.sender);_;
+    }
+    constructor(){
+        _owner=msg.sender;
         gen[1].maxCount=168;
         gen[2].maxCount=1680;//TESTING VARIABLES
     }
-    function ownerOf(uint256 _c)public view override returns(address){
+    function supportsInterface(bytes4 _t)external pure returns(bool){
+        return _t==type(IERC721).interfaceId||_t==type(IERC721Metadata).interfaceId;
+    }
+    function ownerOf(uint256 _c)external view returns(address){
         return owl[_c].owner;
     }
-    function tokenURI(uint256 _c)public view override returns(string memory){
+    function owner()external view returns(address){
+        return _owner;
+    }
+    function name()external pure override returns(string memory){
+        return"Whooli Hootie Conservation Club";
+    }
+    function symbol()external pure override returns(string memory){
+        return"WHCC";
+    }
+    function tokenURI(uint256 _c)external view override returns(string memory){
         return string(abi.encodePacked("ipfs://",owl[_c].cid)); 
     }
-    function transferFrom(address _f,address _t,uint256 _c)public override{
-        unchecked{
-            require(owl[_c].owner==_f);
-            for(uint256 i=0;i<tokens[_f].length;i++)if(tokens[_f][i]==_c){
-                tokens[_f][i]=tokens[_f][tokens[_f].length-1];
-                tokens[_f].pop();
-                break;
-            }
-            tokens[_t].push(_c);
-            owl[_c].parent1=0;
-            owl[_c].parent2=0;
-            owl[_c].owner=_t;
+    function safeTransferFrom(address _f,address _t,uint256 _c)external override{
+        transferFrom(_f,_t,_c);
+    }
+    function safeTransferFrom(address _f,address _t,uint256 _c,bytes memory _d)external override{
+        require(keccak256(abi.encodePacked(_d))==keccak256(abi.encodePacked(_d))); //to dismiss warning (+gasfee)
+        transferFrom(_f,_t,_c);
+    }
+    function transferFrom(address _f,address _t,uint256 _c)public override{unchecked{
+        require(owl[_c].owner==_f);
+        for(uint256 i=0;i<tokens[_f].length;i++)if(tokens[_f][i]==_c){
+            tokens[_f][i]=tokens[_f][tokens[_f].length-1];
+            tokens[_f].pop();
+            break;
         }
+        tokens[_t].push(_c);
+        owl[_c].parent1=0;
+        owl[_c].parent2=0;
+        owl[_c].owner=_t;
         emit Transfer(_f,_t,_c);
+    }}
+    function approve(address _t,uint256 _c)external override{
+        emit Approval(owl[_c].owner,_t,_c);
+    }
+    function getApproved(uint256 _c)external view override returns(address){
+        require(_c==_c); //to dismiss warning (+gasfee)
+        return msg.sender;
     }
     function getWallet(address _a)external view returns(uint256[]memory){
         return tokens[_a];
@@ -57,61 +171,52 @@ contract WHOOLIHOOTIE is ERC721,Ownable{
     function GENPREP(uint256 _c, uint256 _x)external onlyOwner{
         gen[_c].maxCount=_x;
     }
-    function DISTRIBUTE()external onlyOwner{
-        unchecked{
-            for(uint256 i=1;i<=count;i++){
-                (bool _z,)=payable(payable(owl[i].owner)).call{value:address(this).balance/count}("");
-                require(_z);
-            }
+    function DISTRIBUTE()external{unchecked{
+        bool _s;
+        (_s,)=payable(payable(_owner)).call{value:address(this).balance/gen[1].currentCount<168?95:5*100}("");
+        for(uint256 i=1;i<=count;i++){
+            (_s,)=payable(payable(owl[i].owner)).call{value:address(this).balance/count}("");
         }
-    }
-    function _mint(address _a, uint256 _g,uint256 _s,string memory _i)private{
-        unchecked{
-            require(gen[_g].currentCount<gen[_g].maxCount);
-            count++;
-            gen[_g].currentCount++;
-            owl[count].owner=_a;
-            owl[count].sex=_s;
-            owl[count].cid=_i;
-            owl[count].gen=_g;
-            tokens[_a].push(count);
-        }
+        require(_s);
+        
+    }}
+    function _mint(address _a, uint256 _g,uint256 _s,string memory _i)private{unchecked{
+        require(gen[_g].currentCount<gen[_g].maxCount);
+        count++;
+        gen[_g].currentCount++;
+        owl[count].owner=_a;
+        owl[count].sex=_s;
+        owl[count].cid=_i;
+        owl[count].gen=_g;
+        tokens[_a].push(count);
         emit Transfer(address(0),msg.sender,count);
-    }
+    }}
     function AIRDROP(address _a,uint256 _s,string memory _i)external onlyOwner{
         _mint(_a,1,_s,_i);
     }
-    function MINT(uint256 _s,string memory _i)external payable{
-        unchecked{
-            require(msg.value>=0.00 ether); //[DEPLOYMENT SET TO 0.88]
-            _mint(msg.sender,1,_s,_i);
-            (bool _z,)=payable(owner()).call{value:msg.value*19/20}(""); //pay admin 95%
-            require(_z);
-        }
-    }
-    function BREED(uint256 _1,uint256 _2,uint256 _s,string memory _i)external payable{
-        unchecked{
-            require(msg.value>=0.00 ether); //[DEPLOYMENT SET TO 0.02]
-            bool existed;
-            for(uint256 i=0;tokens[msg.sender].length>i;i++){
-                if(((owl[tokens[msg.sender][i]].parent1==_1&&owl[tokens[msg.sender][i]].parent2==_2)||
-                (owl[tokens[msg.sender][i]].parent2==_1&&owl[tokens[msg.sender][i]].parent1==_2))){
-                    existed=true;
-                    break;
-                }
+    function MINT(uint256 _s,string memory _i)external payable{unchecked{
+        require(msg.value>=0.00 ether); //[DEPLOYMENT SET TO 0.88]
+        _mint(msg.sender,1,_s,_i);
+    }}
+    function BREED(uint256 _1,uint256 _2,uint256 _s,string memory _i)external payable{unchecked{
+        require(msg.value>=0.00 ether); //[DEPLOYMENT SET TO 0.02]
+        bool existed;
+        for(uint256 i=0;tokens[msg.sender].length>i;i++){
+            if(((owl[tokens[msg.sender][i]].parent1==_1&&owl[tokens[msg.sender][i]].parent2==_2)||
+            (owl[tokens[msg.sender][i]].parent2==_1&&owl[tokens[msg.sender][i]].parent1==_2))){
+                existed=true;
+                break;
             }
-            require(!existed&& //never mint before
-                owl[_1].gen==owl[_2].gen&& //must be same gen
-                owl[_1].owner==msg.sender&&owl[_2].owner==msg.sender&& //must only owner of _1 and _2
-                (owl[_1].sex==0&&owl[_2].sex==1||owl[_2].sex==0&&owl[_1].sex==1)&& //must be different sex
-                owl[_1].time+0<block.timestamp&&owl[_2].time+0<block.timestamp);//time [DEPLOYMENT 604800]
-            _mint(msg.sender,owl[_1].gen+1,_s,_i);
-            owl[count].parent1=_1;
-            owl[count].parent2=_2;
-            owl[_1].time=block.timestamp;
-            owl[_2].time=block.timestamp;
-            (bool _z,)=payable(owner()).call{value:msg.value/5}(""); //pay admin 20%
-            require(_z);
         }
-    }
+        require(!existed&& //never mint before
+            owl[_1].gen==owl[_2].gen&& //must be same gen
+            owl[_1].owner==msg.sender&&owl[_2].owner==msg.sender&& //must only owner of _1 and _2
+            (owl[_1].sex==0&&owl[_2].sex==1||owl[_2].sex==0&&owl[_1].sex==1)&& //must be different sex
+            owl[_1].time+0<block.timestamp&&owl[_2].time+0<block.timestamp);//time [DEPLOYMENT 604800]
+        _mint(msg.sender,owl[_1].gen+1,_s,_i);
+        owl[count].parent1=_1;
+        owl[count].parent2=_2;
+        owl[_1].time=block.timestamp;
+        owl[_2].time=block.timestamp;
+    }}
 } 
