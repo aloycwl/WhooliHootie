4151ef048248adc0609590f4fbfae35b07061f70
aloycwl <aloycwl@gmail.com>
2022-04-20 19:07:00 +0800

    1 file modified

--- whcc/js/abi.js
+++ whcc/js/abi.js
@@ -1,16 +1,32 @@
 var abi = [
   {
     inputs: [
-      { internalType: 'address', name: 'a', type: 'address' },
-      { internalType: 'uint256', name: 's', type: 'uint256' },
-      { internalType: 'string', name: 'r', type: 'string' },
+      {
+        internalType: 'address',
+        name: 'a',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 's',
+        type: 'uint256',
+      },
+      {
+        internalType: 'string',
+        name: 'r',
+        type: 'string',
+      },
     ],
     name: 'AIRDROP',
     outputs: [],
     stateMutability: 'nonpayable',
     type: 'function',
   },
-  { inputs: [], stateMutability: 'nonpayable', type: 'constructor' },
+  {
+    inputs: [],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
   {
     anonymous: false,
     inputs: [
@@ -51,15 +67,28 @@ var abi = [
         name: 'operator',
         type: 'address',
       },
-      { indexed: false, internalType: 'bool', name: 'approved', type: 'bool' },
+      {
+        indexed: false,
+        internalType: 'bool',
+        name: 'approved',
+        type: 'bool',
+      },
     ],
     name: 'ApprovalForAll',
     type: 'event',
   },
   {
     inputs: [
-      { internalType: 'address', name: 't', type: 'address' },
-      { internalType: 'uint256', name: 'k', type: 'uint256' },
+      {
+        internalType: 'address',
+        name: 't',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'k',
+        type: 'uint256',
+      },
     ],
     name: 'approve',
     outputs: [],
@@ -68,10 +97,26 @@ var abi = [
   },
   {
     inputs: [
-      { internalType: 'uint256', name: 'p', type: 'uint256' },
-      { internalType: 'uint256', name: 'q', type: 'uint256' },
-      { internalType: 'uint256', name: 's', type: 'uint256' },
-      { internalType: 'string', name: 'r', type: 'string' },
+      {
+        internalType: 'uint256',
+        name: 'p',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'q',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 's',
+        type: 'uint256',
+      },
+      {
+        internalType: 'string',
+        name: 'r',
+        type: 'string',
+      },
     ],
     name: 'BREED',
     outputs: [],
@@ -87,8 +132,16 @@ var abi = [
   },
   {
     inputs: [
-      { internalType: 'uint256', name: 'k', type: 'uint256' },
-      { internalType: 'uint256', name: 'm', type: 'uint256' },
+      {
+        internalType: 'uint256',
+        name: 'k',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'm',
+        type: 'uint256',
+      },
     ],
     name: 'GENPREP',
     outputs: [],
@@ -97,8 +150,16 @@ var abi = [
   },
   {
     inputs: [
-      { internalType: 'uint256', name: 's', type: 'uint256' },
-      { internalType: 'string', name: 'r', type: 'string' },
+      {
+        internalType: 'uint256',
+        name: 's',
+        type: 'uint256',
+      },
+      {
+        internalType: 'string',
+        name: 'r',
+        type: 'string',
+      },
     ],
     name: 'MINT',
     outputs: [],
@@ -107,9 +168,21 @@ var abi = [
   },
   {
     inputs: [
-      { internalType: 'address', name: 'f', type: 'address' },
-      { internalType: 'address', name: 't', type: 'address' },
-      { internalType: 'uint256', name: 'k', type: 'uint256' },
+      {
+        internalType: 'address',
+        name: 'f',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 't',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'k',
+        type: 'uint256',
+      },
     ],
     name: 'safeTransferFrom',
     outputs: [],
@@ -118,10 +191,26 @@ var abi = [
   },
   {
     inputs: [
-      { internalType: 'address', name: 'f', type: 'address' },
-      { internalType: 'address', name: 't', type: 'address' },
-      { internalType: 'uint256', name: 'k', type: 'uint256' },
-      { internalType: 'bytes', name: 'd', type: 'bytes' },
+      {
+        internalType: 'address',
+        name: 'f',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 't',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'k',
+        type: 'uint256',
+      },
+      {
+        internalType: 'bytes',
+        name: 'd',
+        type: 'bytes',
+      },
     ],
     name: 'safeTransferFrom',
     outputs: [],
@@ -130,8 +219,16 @@ var abi = [
   },
   {
     inputs: [
-      { internalType: 'address', name: 'p', type: 'address' },
-      { internalType: 'bool', name: 'a', type: 'bool' },
+      {
+        internalType: 'address',
+        name: 'p',
+        type: 'address',
+      },
+      {
+        internalType: 'bool',
+        name: 'a',
+        type: 'bool',
+      },
     ],
     name: 'setApprovalForAll',
     outputs: [],
@@ -139,10 +236,33 @@ var abi = [
     type: 'function',
   },
   {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'a',
+        type: 'address',
+      },
+    ],
+    name: 'TokenAddress',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
     anonymous: false,
     inputs: [
-      { indexed: true, internalType: 'address', name: 'from', type: 'address' },
-      { indexed: true, internalType: 'address', name: 'to', type: 'address' },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'from',
+        type: 'address',
+      },
+      {
+        indexed: true,
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
       {
         indexed: true,
         internalType: 'uint256',
@@ -155,9 +275,21 @@ var abi = [
   },
   {
     inputs: [
-      { internalType: 'address', name: 'f', type: 'address' },
-      { internalType: 'address', name: 't', type: 'address' },
-      { internalType: 'uint256', name: 'k', type: 'uint256' },
+      {
+        internalType: 'address',
+        name: 'f',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 't',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'k',
+        type: 'uint256',
+      },
     ],
     name: 'transferFrom',
     outputs: [],
@@ -165,78 +297,175 @@ var abi = [
     type: 'function',
   },
   {
-    inputs: [{ internalType: 'address', name: 'o', type: 'address' }],
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'o',
+        type: 'address',
+      },
+    ],
     name: 'balanceOf',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'count',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    name: 'gen',
     outputs: [
-      { internalType: 'uint256', name: 'maxCount', type: 'uint256' },
-      { internalType: 'uint256', name: 'currentCount', type: 'uint256' },
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
     ],
     stateMutability: 'view',
     type: 'function',
   },
   {
-    inputs: [{ internalType: 'uint256', name: 'tokenId', type: 'uint256' }],
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    name: 'gen',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: 'maxCount',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'currentCount',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'tokenId',
+        type: 'uint256',
+      },
+    ],
     name: 'getApproved',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'getBalance',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'address', name: 'a', type: 'address' }],
-    name: 'getWallet',
-    outputs: [{ internalType: 'uint256[]', name: '', type: 'uint256[]' }],
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [
-      { internalType: 'address', name: 'o', type: 'address' },
-      { internalType: 'address', name: 'p', type: 'address' },
+      {
+        internalType: 'address',
+        name: 'o',
+        type: 'address',
+      },
+      {
+        internalType: 'address',
+        name: 'p',
+        type: 'address',
+      },
     ],
     name: 'isApprovedForAll',
-    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
     inputs: [],
     name: 'name',
-    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
     stateMutability: 'pure',
     type: 'function',
   },
   {
-    inputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
     name: 'owl',
     outputs: [
-      { internalType: 'address', name: 'owner', type: 'address' },
-      { internalType: 'uint256', name: 'parent1', type: 'uint256' },
-      { internalType: 'uint256', name: 'parent2', type: 'uint256' },
-      { internalType: 'uint256', name: 'time', type: 'uint256' },
-      { internalType: 'uint256', name: 'gen', type: 'uint256' },
-      { internalType: 'uint256', name: 'sex', type: 'uint256' },
-      { internalType: 'string', name: 'cid', type: 'string' },
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'parent1',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'parent2',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'time',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'gen',
+        type: 'uint256',
+      },
+      {
+        internalType: 'uint256',
+        name: 'sex',
+        type: 'uint256',
+      },
+      {
+        internalType: 'string',
+        name: 'cid',
+        type: 'string',
+      },
     ],
     stateMutability: 'view',
     type: 'function',
@@ -244,45 +473,127 @@ var abi = [
   {
     inputs: [],
     name: 'owner',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
-    inputs: [{ internalType: 'uint256', name: 'k', type: 'uint256' }],
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'k',
+        type: 'uint256',
+      },
+    ],
     name: 'ownerOf',
-    outputs: [{ internalType: 'address', name: '', type: 'address' }],
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
   {
-    inputs: [{ internalType: 'bytes4', name: 'f', type: 'bytes4' }],
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'a',
+        type: 'address',
+      },
+    ],
+    name: 'PLAYERITEMS',
+    outputs: [
+      {
+        internalType: 'uint256[]',
+        name: '',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '',
+        type: 'uint256[]',
+      },
+      {
+        internalType: 'uint256[]',
+        name: '',
+        type: 'uint256[]',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'bytes4',
+        name: 'f',
+        type: 'bytes4',
+      },
+    ],
     name: 'supportsInterface',
-    outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
     stateMutability: 'pure',
     type: 'function',
   },
   {
     inputs: [],
     name: 'symbol',
-    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
     stateMutability: 'pure',
     type: 'function',
   },
   {
     inputs: [
-      { internalType: 'address', name: '', type: 'address' },
-      { internalType: 'uint256', name: '', type: 'uint256' },
+      {
+        internalType: 'uint256',
+        name: 'k',
+        type: 'uint256',
+      },
     ],
-    name: 'tokens',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function',
-  },
-  {
-    inputs: [{ internalType: 'uint256', name: 'k', type: 'uint256' }],
     name: 'tokenURI',
-    outputs: [{ internalType: 'string', name: '', type: 'string' }],
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
     stateMutability: 'view',
     type: 'function',
   },
