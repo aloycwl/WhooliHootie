bc344cdafc2e338cfecaedc7d8fb89c5dad60972
aloycwl <aloycwl@gmail.com>
2022-04-21 11:05:47 +0800

    2 files modified

--- whcc/js/abi.backup.js
+++ whcc/js/abi.backup.js
@@ -603,13 +603,3 @@ const abi = [
     type: 'function',
   },
 ];
-
-const abi2 = [
-	{
-		constant: true,
-		inputs: [{ name: '_owner', type: 'address' }],
-		name: 'balanceOf',
-		outputs: [{ name: 'balance', type: 'uint256' }],
-		type: 'function',
-	},
-];
