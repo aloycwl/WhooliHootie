e19125c5b037511190bb21e25703b57596622b40
aloycwl <aloycwl@gmail.com>
2022-04-25 17:46:18 +0800

    1 file modified

--- od/js/load.js
+++ od/js/load.js
@@ -1,38 +1,43 @@
 //wood, metal, food, owl, soldier [DEVELOPMENT: set minting fee]
-var acct, res, items, img, loaded;
-
+var acct, res, items, img, loaded, s;
 async function loadNFTs() {
   res = await contract.player(acct[0]).call();
-  $('#resources').html(
-    `Wood: ${res[0]}<br>Metal: ${res[1]}<br>Food: ${res[2]}<br>Owl: ${res[3]}<br>Soldier: ${res[4]}`
-  );
   items = await contract.PLAYERITEMS(acct[0]).call();
   await $.getJSON('js/img.min.json', function (d) {
     img = d;
   });
+  s += '<br>Random addresses:<br>';
+  //while lo
+  //get the count
+  //get random addresses
+  //check if it is not the same as owner
   for (let i = 0; i < items[0].length; i++)
-    $('#nfts').append(
-      `<img src="https://ipfs.io/ipfs/${img[items[0][i]][items[1][i]]}"> `
-    );
+    s += `<img src=https://ipfs.io/ipfs/${img[items[0][i]][items[1][i]]}> `;
+  $('#res').html(
+    `Wood: ${res[0]} | Metal: ${res[1]} | Food: ${res[2]} | Owl: ${res[3]} | Soldier: ${res[4]}<br>${s}`
+  );
+}
+function r() {
+  location.reload();
 }
 async function MINT() {
   await contract.MINT($('#items').val()).send({
     from: acct[0],
     value: 0.0e18,
   });
-  location.reload();
+  r();
 }
 async function CLAIM() {
   await contract.CLAIM(acct[0]).send({
     from: acct[0],
   });
-  location.reload();
+  r();
 }
 async function ATTACK() {
   await contract.CLAIM($('#txtAtk').val()).send({
     from: acct[0],
   });
-  location.reload();
+  r();
 }
 async function load() {
   web3 = new Web3(ethereum);
@@ -43,7 +48,7 @@ async function load() {
       method: 'wallet_switchEthereumChain',
       params: [{ chainId: '0x4' }],
     });
-    location.reload();
+    r();
   } else {
     contract = new web3.Contract(
       abi,
