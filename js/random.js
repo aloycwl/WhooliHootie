b0a07f822d818ddeb2c60808316005efa02ff8a5
aloycwl <aloycwl@gmail.com>
2022-05-20 22:28:14 +0800

    1 file modified

--- js/random.js
+++ js/random.js
@@ -1,11 +1,11 @@
 c = document.getElementById('myCanvas');
 cd = c.getContext('2d');
-async function draw(s1, s2, s3) {
-  s2 = ran(s2);
-  if (s3 <= s2) {
+async function dd(s1, s2, s3) {
+  r = ran(s2);
+  if (r <= s3) {
     img = new Image();
     img.setAttribute('crossorigin', 'anonymous');
-    img.src = `https://aloycwl.github.io/twc_frontend/img/${s1}/${s2}.png`;
+    img.src = `https://aloycwl.github.io/twc_frontend/img/${s1}/${r}.png`;
     return new Promise((resolve) => {
       img.onload = function () {
         cd.drawImage(img, 0, 0, 350, 350);
@@ -16,8 +16,8 @@ async function draw(s1, s2, s3) {
 }
 async function load() {
   //sex = ran(3);
-  await draw(0, 10, 9);
-  await draw(1, 9, 9);
+  await dd(0, 9, 9);
+  await dd(1, 9, 9);
 }
 function ran(p) {
   return Math.floor(Math.random() * p);
