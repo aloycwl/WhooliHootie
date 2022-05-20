e5d115c5838d2c89f0b2d3205d9287020d746c47
Aloysius Chan <aloycwl@gmail.com>
2022-05-20 22:10:16 +0800

    Update random.js

--- js/random.js
+++ js/random.js
@@ -1,23 +1,24 @@
 c = document.getElementById('myCanvas');
 cd = c.getContext('2d');
-async function draw(s1, s2) {
-  img = new Image();
-  img.setAttribute('crossorigin', 'anonymous');
-  img.src = `https://aloycwl.github.io/twc_frontend/img/${s1}/${s2}.png`;
-  return new Promise((resolve) => {
-    img.onload = function () {
-      cd.drawImage(img, 0, 0, 350, 350);
-      resolve();
-    };
-  });
+async function draw(s1, s2, s3) {
+  Math.floor(Math.random() * s2);
+  if (s3 < s2) {
+    img = new Image();
+    img.setAttribute('crossorigin', 'anonymous');
+    img.src = `https://aloycwl.github.io/twc_frontend/img/${s1}/${s2}.png`;
+    return new Promise((resolve) => {
+      img.onload = function () {
+        cd.drawImage(img, 0, 0, 350, 350);
+        resolve();
+      };
+    });
+  }
 }
 async function load() {
-  s2 = ran(10);
-  if (s2 < 9) await draw(0, s2);
-  s2 = ran(12);
-  if (s2 < 11) draw(1, s2);
+  await draw(0, 10, 9);
+  await draw(1, 9, 9);
 }
 function ran(p) {
-  return Math.floor(Math.random() * p);
+  return;
 }
 load();
