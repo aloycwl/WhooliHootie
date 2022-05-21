0c438d09afb34d0d96a9897a240adc4d0eec6f82
aloycwl <aloycwl@gmail.com>
2022-05-21 10:43:27 +0800

    2 files modified

--- js/random.js
+++ js/random.js
@@ -1,8 +1,36 @@
-c = document.getElementById('myCanvas');
+const Background = [
+    'Green',
+    'Pink',
+    'Brown',
+    'Dark Gray',
+    'Purple',
+    'Gray',
+    'Yellow',
+    'Red',
+    'Blue',
+  ],
+  Body = [
+    'Blue',
+    'Pink',
+    'Light Gray',
+    'Green',
+    'Red',
+    'Dark Gray',
+    'Brown',
+    'Purple',
+    'Yellow',
+  ],
+  Headwear = ['Black Headphones', 'White Headphones', 'Beret', 'Tophat'],
+  Brow = ['Furrowed', 'Raised', 'Normal'],
+  Eyes = ['Sore', 'Normal', 'Crazy', 'Tired', 'Blur', 'Happy', 'Flame'],
+  Beak = ['Open', 'Sad', 'Gold Teeth', 'Tounge Out', 'Wide'],
+  c = document.getElementById('myCanvas');
 cd = c.getContext('2d');
-async function dd(s1, s2, s3) {
+
+async function dd(s1, s2, s3, s4) {
   r = ran(s2);
   if (r <= s3) {
+    $('#traits').append(`{"trait_type":"${s1}","value":"${s4[r]}"}`);
     img = new Image();
     img.setAttribute('crossorigin', 'anonymous');
     img.src = `https://aloycwl.github.io/twc_frontend/img/${s1}/${r}.png`;
@@ -15,9 +43,10 @@ async function dd(s1, s2, s3) {
   }
 }
 async function load() {
-  //sex = ran(3);
-  await dd(0, 9, 9);
-  await dd(1, 9, 9);
+  $('#traits').html(`"attributes": [`);
+  sex = ran(3);
+  await dd('Background', 9, 9, Background);
+  await dd('Body', 9, 9, Body);
 }
 function ran(p) {
   return Math.floor(Math.random() * p);
